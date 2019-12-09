2c27bb4095cdace95ee8c652fe2f3394bc042dc0
yagray <yagray@mail.ru>
2019-12-09 14:26:20 +0300

    GUI внутри активити динамически появляется при старте и динамически исчезает при нажатии на кнопку ENTER

--- app/src/main/java/k/s/yarlykov/motionedu/MainActivity.kt
+++ app/src/main/java/k/s/yarlykov/motionedu/MainActivity.kt
@@ -1,12 +1,24 @@
 package k.s.yarlykov.motionedu
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonOk.setOnClickListener {
+            (motionLayout as MotionLayout).transitionToStart()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (motionLayout as MotionLayout).transitionToEnd()
     }
 }
