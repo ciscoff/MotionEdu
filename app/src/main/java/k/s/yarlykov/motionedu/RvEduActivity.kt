f7a22c1e148324ee0af03bcae07c4ce9021bc1c9
yagray <yagray@mail.ru>
2020-01-13 12:38:32 +0300

    В итоге получил нужный результат с помощью translationY

--- app/src/main/java/k/s/yarlykov/motionedu/RvEduActivity.kt
+++ app/src/main/java/k/s/yarlykov/motionedu/RvEduActivity.kt
@@ -21,25 +21,25 @@ import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.Observable
 import io.reactivex.subjects.BehaviorSubject
 import k.s.yarlykov.motionedu.custom.ParallaxListItem
+import k.s.yarlykov.motionedu.custom.ParallaxListItem.Companion.INVALID_MAX_OFFSET
 import k.s.yarlykov.motionedu.custom.VerticalMotionListener
 import kotlinx.android.synthetic.main.activity_rv_edu.*
 
 class RvEduActivity : AppCompatActivity() {
 
-//    val imagesID = listOf(
-//        R.drawable.bkg_01_jan,
-//        R.drawable.bkg_02_feb,
-//        R.drawable.bkg_03_mar,
-//        R.drawable.bkg_04_apr,
-//        R.drawable.bkg_05_may
-//    )
-
     val imagesID = listOf(
-        R.drawable.clip_bkg_01_jan,
-        R.drawable.clip_bkg_02_feb,
-        R.drawable.clip_bkg_03_mar,
-        R.drawable.clip_bkg_04_apr,
-        R.drawable.clip_bkg_05_may
+        R.drawable.bkg_00_dec,
+        R.drawable.bkg_01_jan,
+        R.drawable.bkg_02_feb,
+        R.drawable.bkg_03_mar,
+        R.drawable.bkg_04_apr,
+        R.drawable.bkg_05_may,
+        R.drawable.bkg_06_jun,
+        R.drawable.bkg_07_jul,
+        R.drawable.bkg_08_aug,
+        R.drawable.bkg_09_sep,
+        R.drawable.bkg_10_oct,
+        R.drawable.bkg_11_nov
     )
 
     val subject: BehaviorSubject<Int> = BehaviorSubject.create()
@@ -61,16 +61,17 @@ class RvEduActivity : AppCompatActivity() {
             addOnScrollListener(object : RecyclerView.OnScrollListener() {
                 override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
 
+                    /**
+                     * Как я понял в dy (dx) получаем "длину" проделанного скрола.
+                     * Она может быть отрицательной и положительной в зависимости от направления
+                     *
+                     * dy > 0 [Scrolled Downwards] Крутим к нижнему элементу списка
+                     * dy < 0 [Scrolled Upwards] Крутим в начало, к верхнему элементу
+                     */
+
                     if (dy != 0) {
                         subject.onNext(dy)
                     }
-//                    if (dy > 0) {
-//                        System.out.println("APP_TAG: Scrolled Downwards y=${dy}")
-//                    } else if (dy < 0) {
-//                        System.out.println("APP_TAG: Scrolled Upwards y=${dy}")
-//                    } else {
-//                        System.out.println("APP_TAG: No Vertical Scrolled y=${dy}")
-//                    }
                 }
 
                 override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
@@ -104,13 +105,10 @@ class RvEduActivity : AppCompatActivity() {
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             holder.bind(imagesID[position])
-//            holder.itemView.setOnClickListener { view ->
-//                explodeOnClick(view)
-//            }
         }
 
         inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-            private var maxY : Float = 0f
+            private var maxY : Float = INVALID_MAX_OFFSET
             private val ivMotion = view.findViewById<ImageView>(R.id.ivMotion)
             private val tvMotion = view.findViewById<TextView>(R.id.tvMotion)
             private val motionView : VerticalMotionListener
@@ -119,12 +117,25 @@ class RvEduActivity : AppCompatActivity() {
             init {
                 val d = scrollObservable.subscribe {
 
-                    if(view.y > maxY) {
+                    if(adapterPosition == 0) {
+                        motionView.setDebugFlag(true)
+                    } else {
+                        motionView.setDebugFlag(false)
+                    }
+
+
+                    if(view.y >= maxY) {
                         maxY = view.y
                         motionView.onUpdateMaxOffset(maxY)
                     }
 
+                    if(adapterPosition == 0) {
+                        System.out.println("APP_TAG: view.y=${view.y}")
+                        System.out.println("APP_TAG: maxY=${maxY}")
+                    }
+
                     motionView.onOffsetChanged(view.y)
+
 
 //                    if(adapterPosition == 1) {
 //                        System.out.println("APP_TAG: ItemYPosition y=${view.y}")
@@ -136,6 +147,7 @@ class RvEduActivity : AppCompatActivity() {
             fun bind(imageId: Int) {
                 ivMotion.setImageResource(imageId)
                 tvMotion.text = context.getString(R.string.motion)
+                maxY = INVALID_MAX_OFFSET
             }
         }
     }
--- app/src/main/java/k/s/yarlykov/motionedu/custom/ParallaxListItem.kt
+++ app/src/main/java/k/s/yarlykov/motionedu/custom/ParallaxListItem.kt
@@ -11,36 +11,49 @@ class ParallaxListItem @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : MotionLayout(context, attrs, defStyleAttr), VerticalMotionListener {
 
-    private var maxOffset = 0f
-    lateinit var clipDrawable : ClipDrawable
+    companion object {
+        val INVALID_MAX_OFFSET = Float.NEGATIVE_INFINITY
+    }
+
+    private var maxOffset = INVALID_MAX_OFFSET
+    private var interpolationLength = 0f
+    private var isShowProgress = false
 
     override fun onOffsetChanged(offset: Float) {
 
+        if (isShowProgress) {
+            System.out.println("APP_TAG: ${hashCode().toString(16)}: interpolationLength=$interpolationLength, maxOffset=$maxOffset, offset=$offset, height=$height, progress=$progress")
+        }
 
-        if(maxOffset != 0f && offset >= 0) {
-            System.out.println("APP_TAG: ${hashCode().toString(16)}. maxOffset=$maxOffset, offset=$offset")
+        if (maxOffset != INVALID_MAX_OFFSET && interpolationLength != 0f && offset >= -height) {
+            progress = (interpolationLength - (height.toFloat() + offset)) / interpolationLength
 
-            progress = Math.abs((maxOffset - offset)/maxOffset)
-            clipDrawable.level = (Math.abs((maxOffset - offset)/maxOffset) * 10000f).toInt()
+//            if (isShowProgress) {
+//                System.out.println("APP_TAG: ${hashCode().toString(16)}: interpolationLength=$interpolationLength, maxOffset=$maxOffset, offset=$offset, height=$height, progress=$progress")
+//            }
 
-            System.out.println("APP_TAG: ${hashCode().toString(16)}. level=${clipDrawable.level}")
+
+//            progress = Math.abs((maxOffset - offset)/maxOffset)
+//            clipDrawable.level = (Math.abs((maxOffset - offset)/maxOffset) * 10000f).toInt()
+//
+//            System.out.println("APP_TAG: ${hashCode().toString(16)}. level=${clipDrawable.level}")
 //            ((maxOffset - offset)/maxOffset).toInt()
-
-
-
-
 //            System.out.println("APP_TAG: clipDrawable.level y=${Math.abs((maxOffset - offset)/maxOffset).toInt() * 10000}")
-
         }
     }
 
+    override fun setDebugFlag(flag: Boolean) {
+        isShowProgress = flag
+    }
+
     override fun onUpdateMaxOffset(maxOffset: Float) {
-        clipDrawable = this.findViewById<ImageView>(R.id.ivMotion).drawable as ClipDrawable
         this.maxOffset = maxOffset
+        this.interpolationLength = maxOffset + height
     }
 }
 
 interface VerticalMotionListener {
-    fun onOffsetChanged(offset : Float)
-    fun onUpdateMaxOffset(maxOffset : Float)
+    fun onOffsetChanged(offset: Float)
+    fun onUpdateMaxOffset(maxOffset: Float)
+    fun setDebugFlag(flag: Boolean)
 }
